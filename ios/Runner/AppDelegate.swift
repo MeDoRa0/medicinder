7f5dd61659b6386aeff6055c0feb03601eb824bb
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2025-11-18 14:39:00 +0200

    add ios premissions

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import UserNotifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,26 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    configureNotificationPermissions(application)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func configureNotificationPermissions(_ application: UIApplication) {
+    let notificationCenter = UNUserNotificationCenter.current()
+    notificationCenter.delegate = self
+
+    notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+      if let error = error {
+        NSLog("Notification permission error: \(error.localizedDescription)")
+        return
+      }
+      NSLog("Notification permission granted: \(granted)")
+      if granted {
+        DispatchQueue.main.async {
+          application.registerForRemoteNotifications()
+        }
+      }
+    }
   }
 }
