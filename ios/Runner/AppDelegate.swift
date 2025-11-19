02140a0434528fa45646f20c6a4535ade52fa3a7
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2025-11-19 14:31:27 +0200

    modifiy files for ios build

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,33 +1,28 @@
-import Flutter
 import UIKit
+import Flutter
 import UserNotifications
+import awesome_notifications
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    configureNotificationPermissions(application)
     GeneratedPluginRegistrant.register(with: self)
+
+    // Initialize Awesome Notifications
+    AwesomeNotifications().initialize(
+      nil,
+      [
+        "channelKey": "basic_channel",
+        "channelName": "Basic notifications",
+        "channelDescription": "Notification tests"
+      ]
+    )
+
+    UNUserNotificationCenter.current().delegate = self
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  private func configureNotificationPermissions(_ application: UIApplication) {
-    let notificationCenter = UNUserNotificationCenter.current()
-    notificationCenter.delegate = self
-
-    notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-      if let error = error {
-        NSLog("Notification permission error: \(error.localizedDescription)")
-        return
-      }
-      NSLog("Notification permission granted: \(granted)")
-      if granted {
-        DispatchQueue.main.async {
-          application.registerForRemoteNotifications()
-        }
-      }
-    }
   }
 }
