d9a240b251bc7ae3c04748e996136fd332a2618d
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2026-02-18 02:08:56 +0200

    chore: Prepare for Codemagic build - add Podfile and ignore backups

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,28 +1,13 @@
-import UIKit
 import Flutter
-import UserNotifications
-import awesome_notifications
+import UIKit
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-
-    // Initialize Awesome Notifications
-    AwesomeNotifications().initialize(
-      nil,
-      [
-        "channelKey": "basic_channel",
-        "channelName": "Basic notifications",
-        "channelDescription": "Notification tests"
-      ]
-    )
-
-    UNUserNotificationCenter.current().delegate = self
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
